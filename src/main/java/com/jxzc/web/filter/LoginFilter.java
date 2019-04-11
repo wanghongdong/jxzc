200a9c652594e7795584bb66af4b9f4b5fe6c5b8
wanghongdong <379125868@qq.com>
2019-04-11 22:14:35 +0800

    高德地图api接入

--- src/main/java/com/jxzc/web/filter/LoginFilter.java
+++ src/main/java/com/jxzc/web/filter/LoginFilter.java
@@ -1,15 +1,10 @@
 package com.jxzc.web.filter;
 
-import com.alibaba.druid.util.StringUtils;
-import com.jxzc.web.entity.User;
-import com.jxzc.web.utils.JSConstant;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -33,20 +28,20 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String path = request.getServletPath();
-        if (!StringUtils.isEmpty(path) && (path.startsWith("/index") || path.startsWith("/css") || path.startsWith("/js"))){
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//        String path = request.getServletPath();
+//        if (!StringUtils.isEmpty(path) && (path.startsWith("/index") || path.startsWith("/css") || path.startsWith("/js"))){
+//            filterChain.doFilter(servletRequest,servletResponse);
+//            return;
+//        }
+//        User user = (User) request.getSession().getAttribute(JSConstant.SESSION_CURRENT_USER);
+//        if (user==null){
+//            response.sendRedirect("/index/login");
+//            return;
+//        }else{
             filterChain.doFilter(servletRequest,servletResponse);
-            return;
-        }
-        User user = (User) request.getSession().getAttribute(JSConstant.SESSION_CURRENT_USER);
-        if (user==null){
-            response.sendRedirect("/index/login");
-            return;
-        }else{
-            filterChain.doFilter(servletRequest,servletResponse);
-        }
+//        }
     }
 
     @Override
